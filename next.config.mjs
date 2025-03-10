8ef8896f0a8ef7181459a06fbe70edc912cb5762
Alex Bezdieniezhnykh <zxsanny@gmail.com>
2025-03-10 09:11:50 +0200

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   images: {
     unoptimized: true,
@@ -22,27 +14,3 @@ const nextConfig = {
     parallelServerCompiles: true,
   },
 }
-
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
