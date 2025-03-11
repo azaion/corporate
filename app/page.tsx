8a2b6092fe0415e73aabdd9eeea5c2c0e0deaef2
Dennis Popov <denys.popov@gmail.com>
2025-03-11 21:28:58 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -288,7 +288,7 @@ export default function Home() {
               <div className="w-full max-w-sm space-y-2">
                 <form className="flex flex-col space-y-4">
                   <input
-                    className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                    className="text-blue-800 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                     placeholder="Your email"
                     type="email"
                   />
