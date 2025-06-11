70215522d6c0ef7d697fed45f06788c013351d2e
Alex Bezdieniezhnykh <zxsanny@gmail.com>
2025-06-11 22:23:53 +0300

    fix images - import component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight, Cpu, Globe, Lock, TargetIcon, Zap } from "lucide-react"
 import Link from "next/link"
+import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
